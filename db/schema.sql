566ad3977d9836222e7cb80dbf225ef1d484dce1
Blaine Curren <blaine.curren@gmail.com>
2021-10-02 15:54:54 -0500

    added connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,26 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
-USE employees;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
-CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    department_name VARCHAR(30) NOT NULL
+CREATE TABLE departments(
+id INT AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(30)
 );
 
-CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
-    department_id INTEGER
+CREATE TABLE roles (
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL(8,2),
+department_id INT,
+FOREIGN KEY(department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER
+CREATE TABLE employees(
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+FOREIGN KEY(role_id) REFERENCES roles(id),
+FOREIGN KEY(manager_id) REFERENCES employees(id)
 );
